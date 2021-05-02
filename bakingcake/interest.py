92c8c3c38c5b0affdd03c61ff6a0bd49eca3de74
Daniel Alessandrini <alessandrinil@gmail.com>
2021-05-02 00:21:49 -0400

    Added interest calculation.

--- bakingcake/interest.py
+++ bakingcake/interest.py
@@ -29,11 +29,38 @@ def load_holdings(input_path):
         holdings["holdings"][i]["price"] = CG_API.get_price(
             ids=holdings["holdings"][i]["id"], vs_currencies='usd'
         )[holdings["holdings"][i]["id"]]["usd"]
+        # Calculate the total holdings in USD
         holdings["holdings"][i]["total"] = (
             holdings["holdings"][i]["price"] *
             holdings["holdings"][i]["quantity"])
+        # Calculate the 1 year yield
+        holdings["holdings"][i]["1_year_yield_usd"] = (
+            holdings["holdings"][i]["total"] *
+            (holdings["holdings"][i]["apy"] / 100.0))
+        holdings["holdings"][i]["1_year_yield_token"] = (
+            holdings["holdings"][i]["quantity"] *
+            (holdings["holdings"][i]["apy"] / 100.0))
 
     return holdings
+
+
+def calculate_portfolio_yield(holdings):
+    """
+        This method will calculate the overall yield of the portfolio.
+
+        Args:
+            holdings (dict): portfolio holdings
+
+        Returns:
+            portfolio yield
+    """
+    portfolio_yield = {}
+    portfolio_yield["one_year_yield"] = sum(
+        [i["1_year_yield_usd"] for i in holdings["holdings"]])
+    portfolio_yield["one_day_yield"] = (
+        portfolio_yield["one_year_yield"] / 365.0)
+
+    return portfolio_yield
 
 
 def get_token_info(ticker):
@@ -57,7 +84,10 @@ def get_token_info(ticker):
             i = 0
             while True:
                 token_info = token_list[i]
-                if "token" in token_info["id"]:
+                if (
+                    "governance" not in token_info["id"] and
+                    "-" in token_info["id"]
+                ):
                     i += 1
                 else:
                     break
